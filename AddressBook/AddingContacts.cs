1caf13e5a9a2c64a7e725d5efa0b2fab4b126da9
Nepolean23 <nlean1996@gmail.com>
2023-01-12 11:53:19 +0530

    [NEPOLEAN]-Code for adding of person details in addressbook

---
+++ AddressBook/AddingContacts.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AddingContacts
+    {
+        public static void PersonDetails()
+        {
+            CreatingContacts contacts = new CreatingContacts();
+            Console.WriteLine("Enter your FirstName :");
+            contacts.firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your LastName :");
+            contacts.lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter your PhoneNumber :");
+            contacts.phoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter your Email-Id :");
+            contacts.email = Console.ReadLine();
+
+            Console.WriteLine("Enter your Address :");
+            contacts.address = Console.ReadLine();
+
+            Console.WriteLine("Enter your City :");
+            contacts.city = Console.ReadLine();
+
+            Console.WriteLine("Enter your Sate :");
+            contacts.state = Console.ReadLine();
+
+            Console.WriteLine("Enter your ZipCode :");
+            contacts.zipcode = Convert.ToInt32(Console.ReadLine());
+
+            
+        }
+    }
+}
